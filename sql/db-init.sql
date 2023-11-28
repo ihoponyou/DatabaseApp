c92f6def6c8f4c5cb32e64281e8f22dcbea0efbd
noahmagcalas <noah.magcalas@gmail.com>
2023-11-28 12:01:12 -0600

    Create db-init.sql
    apparently this was due yesterday

---
+++ sql/db-init.sql
@@ -0,0 +1,63 @@
+
+-- CREATE TABLES
+
+CREATE TABLE IF NOT EXISTS Publisher (
+	publisher_id SERIAL,
+	publisher_name VARCHAR(50) UNIQUE NOT NULL,
+	publisher_description text(250),
+	
+	PRIMARY KEY (publisher_id)
+);
+
+CREATE TABLE IF NOT EXISTS Tag (
+	tag_id SERIAL,
+	tag_name VARCHAR(50) UNIQUE NOT NULL,
+	tag_description TEXT(250),
+	
+	PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS Game (
+	game_id SERIAL,
+	publisher_id INT NOT NULL,
+	game_title VARCHAR(50) NOT NULL,
+	game_description TEXT(250),
+	release_date DATE NOT NULL,
+	game_price MONEY NOT NULL,
+	
+	PRIMARY KEY (game_id),
+	FOREIGN KEY (publisher_id) REFERENCES Publisher (publisher_id)
+);
+
+CREATE TABLE IF NOT EXISTS TaggedAs (
+	game_id INT NOT NULL,
+	tag_id INT NOT NULL,
+	
+	PRIMARY KEY (game_id, genre_id),
+	FOREIGN KEY (game_id) REFERENCES Game (game_id),
+	FOREIGN KEY (tag_id) REFERENCES Tag (tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS Customer (
+	customer_id SERIAL,
+	customer_username VARCHAR(32) UNIQUE NOT NULL,
+	customer_password VARCHAR(64) NOT NULL,
+	join_date DATE NOT NULL,
+	
+	PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Review (
+	review_id SERIAL,
+	author_id INT NOT NULL,
+	game_id INT NOT NULL,
+	review_text TEXT(250),
+	review_is_positive BOOL NOT NULL,
+	post_date DATE NOT NULL,
+	
+	PRIMARY KEY (review_id),
+	FOREIGN KEY (author_id) REFERENCES Customer (customer_id),
+	FOREIGN KEY (game_id) REFERENCES Game (game_id)
+);
+
+-- INSERT EXAMPLE DATA
